d33138495595764eaea481fcf86d4403d0ce3464
SkyLearn <ba_de@mail.ru>
2023-01-31 20:59:01 +0300

    Ready2

--- DZ3001_2023/Zadacha5/Program.cs
+++ DZ3001_2023/Zadacha5/Program.cs
@@ -4,53 +4,47 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
-int row = 4;
-int column = 4;
-int[,] table = new int[row, column];
-int number = 1;
-int circle;
-for (circle = 1; circle <= 2; circle++)
+void FillSpiralArray(int[,] array)
 {
-    int right = (-1 + circle);
-    int down = (-1 + circle);
-    int left = row - 1;
-    int up = column - 1;
-    while (up > (-1 + circle))
+    int left = 0, right = array.GetLength(1) - 1, top = 0, bottom = array.GetLength(0) - 1, count = 1;
+    while (count <= array.Length)
     {
-        while (left > (-1 + circle))
-        {
-            while (down < row - 1)
-            {
-                while (right < column - 1)
-                {
-                    table[down, right] = number;
-                    number++;
-                    right++;
-                }
-                table[down, right] = number;
-                number++;
-                down++;
-            }
-            table[down, left] = number;
-            number++;
-            left--;
-        }
+        // слева напрово
+        // Console.WriteLine($"{left} <= {right}");
+        for (int i = left; i <= right; i++)
+            array[top, i] = count++;
+        top++;
 
-        table[up, left] = number;
-        number++;
-        up--;
+        // сверху вниз
+        // Console.WriteLine($"{top} <= {bottom}");
+        for (int i = top; i <= bottom; i++)
+            array[i, right] = count++;
+        right--;
+
+        // справо налево
+        // Console.WriteLine($"{right} >= {left}");
+        for (int i = right; i >= left; i--)
+            array[bottom, i] = count++;
+        bottom--;
+
+        // снизу вверх
+        // Console.WriteLine($"{bottom} >= {top}");
+        for (int i = bottom; i >= top; i--)
+            array[i, left] = count++;
+        left++;
     }
-
-    row = row - circle;
-    column = column - circle;
 }
-
-for (int z = 0; z < (row + circle); z++)
+void PrintArray(int[,] array)
 {
-    for (int q = 0; q < (column + circle); q++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        if (table[z, q] < 10) Console.Write("0" + table[z, q] + "  ");
-        else Console.Write(table[z, q] + "  ");
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
     }
-    Console.WriteLine();
 }
+int rows = 7;
+int columns = 5;
+int[,] massive = new int[rows, columns];
+FillSpiralArray(massive);
+PrintArray(massive);
